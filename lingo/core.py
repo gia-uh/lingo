8d050649664bbfb4734ff86fa1ad6e3b65db8da7
Alejandro Piad <apiad@apiad.net>
2025-12-05 12:51:44 -0500

    Make conversation a protocol

--- lingo/core.py
+++ lingo/core.py
@@ -1,16 +1,18 @@
-from typing import Callable, Coroutine
-from pydantic import BaseModel
-
-from lingo.utils import tee
-
+from typing import Callable, Coroutine, Iterator, Protocol
 from .flow import Flow, flow
-from .llm import LLM, Message  # StreamType is removed
+from .llm import LLM, Message
 from .tools import Tool, tool
 from .context import Context
 from .prompts import DEFAULT_SYSTEM_PROMPT
 from .engine import Engine
 
-import asyncio
+
+class Conversation(Protocol):
+    def append(self, message: Message, /): ...
+    def __iter__(self) -> Iterator[Message]: ...
+    def __getitem__(self, index: int, /) -> Message: ...
+    def clear(self): ...
+    def __len__(self) -> int: ...
 
 
 class Lingo:
@@ -23,6 +25,7 @@ class Lingo:
         tools: list[Tool] | None = None,
         system_prompt: str = DEFAULT_SYSTEM_PROMPT,
         verbose: bool = False,
+        conversation: Conversation | None = None,
     ) -> None:
         self.name = name
         self.description = description
@@ -32,7 +35,9 @@ class Lingo:
         self.llm = llm or LLM()
         self.skills: list[Flow] = skills or []
         self.tools: list[Tool] = tools or []
-        self.messages: list[Message] = []
+        self.messages: Conversation = (
+            conversation if conversation is not None else list[Message]()
+        )
         self.verbose = verbose
 
     def skill(self, func: Callable[[Context, Engine], Coroutine]):
@@ -62,11 +67,13 @@ class Lingo:
     async def chat(self, msg: str) -> Message:
         self.messages.append(Message.user(msg))
 
-        context = Context(self.messages)
+        context = Context(list(self.messages))
         engine = Engine(self.llm, self.tools)
         flow = self._build_flow()
 
         await flow.execute(context, engine)
 
-        self.messages = context.messages
+        for m in context.messages[len(self.messages):]:
+            self.messages.append(m)
+
         return self.messages[-1]
