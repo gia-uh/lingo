95a3588687c7edfc7184cc8dd4a3847c47bb600a
Alejandro Piad <apiad@apiad.net>
2026-01-05 09:30:41 -0500

    Add embedder

--- lingo/__init__.py
+++ lingo/__init__.py
@@ -2,6 +2,7 @@ from .context import Context
 from .core import Lingo
 from .flow import Flow, flow
 from .llm import LLM, Message
+from .embed import Embedder
 from .tools import tool
 from .engine import Engine
 
@@ -14,6 +15,7 @@ __all__ = [
     "Flow",
     "Lingo",
     "LLM",
+    "Embedder",
     "Message",
     "tool",
 ]
